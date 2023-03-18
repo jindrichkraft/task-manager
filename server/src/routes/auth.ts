a0269601f9f8037af3cbdac13abf31426c0dad82
Jindřich Kraft <jindrich.kraft@proton.me>
2023-03-18 14:21:08 +0100

    BE: Creating the /register route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,61 @@
 import { Router } from 'express';
+import bcrypt from 'bcrypt';
+
+import { pool } from '../main';
+import {
+  PASSWORD_SALT_ROUNDS,
+  BAD_REQUEST_CODE,
+  UNAUTHORIZED_CODE,
+  SERVER_ERROR_CODE,
+} from '../utils/constants';
 
 const authRouter = Router();
+
+authRouter.post('/register', async (req, res) => {
+  try {
+    const { username, displayName, password } = req.body;
+    if (!username || !displayName || !password)
+      return res.status(BAD_REQUEST_CODE).json({
+        success: false,
+        message: 'Invalid input data!',
+        data: null,
+      });
+
+    const existingUser = await pool.query(
+      'SELECT * FROM Users WHERE LOWER(username) = LOWER($1)',
+      [username.toLowerCase()]
+    );
+    if (existingUser.rowCount !== 0)
+      return res.status(UNAUTHORIZED_CODE).json({
+        success: false,
+        message: 'User with this username already exists!',
+        data: null,
+      });
+
+    const salt = await bcrypt.genSalt(PASSWORD_SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = (
+      await pool.query(
+        'INSERT INTO Users (username, display_name, password) VALUES ($1, $2, $3) RETURNING *',
+        [username, displayName, hashedPassword]
+      )
+    ).rows[0];
+
+    return res.json({
+      success: true,
+      message: 'Successfully created user!',
+      data: {
+        id: newUser.user_id,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(SERVER_ERROR_CODE).json({
+      success: false,
+      message: 'Server error!',
+      data: null,
+    });
+  }
+});
 
 export default authRouter;
--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -3,3 +3,11 @@ export const DEFAULT_EXPRESS_PORT = 3001;
 export const DEFAULT_POSTGRES_PORT = 5432;
 
 export const DEFAULT_POSTGRES_HOST = 'localhost';
+
+// Auth
+export const PASSWORD_SALT_ROUNDS = 10;
+
+// Error Codes
+export const BAD_REQUEST_CODE = 400;
+export const UNAUTHORIZED_CODE = 401;
+export const SERVER_ERROR_CODE = 500;
