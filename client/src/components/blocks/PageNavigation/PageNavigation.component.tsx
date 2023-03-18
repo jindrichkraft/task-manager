94a7e9fd24cebad920e5367b85ca443a2f735e39
Jindřich Kraft <jindrich.kraft@proton.me>
2023-03-18 20:51:08 +0100

    FE: Creating the Profile and Logout pages

--- client/src/components/blocks/PageNavigation/PageNavigation.component.tsx
+++ client/src/components/blocks/PageNavigation/PageNavigation.component.tsx
@@ -1,17 +1,39 @@
 import { Link } from 'react-router-dom';
 
-const PageNavigation = (): JSX.Element => (
-  <nav>
-    <h2>Page Navigation</h2>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/login">Sign in</Link>
-      </li>
-    </ul>
-  </nav>
-);
+import { useAuth } from '../../../hooks/auth';
+
+const PageNavigation = (): JSX.Element => {
+  const { auth } = useAuth();
+
+  return (
+    <nav>
+      <h2>Page Navigation</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {auth ? (
+          <>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/logout"> Logout</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
 
 export default PageNavigation;
---
+++ client/src/pages/ProfilePage.tsx
@@ -0,0 +1,9 @@
+import DefaultLayout from '../layouts/DefaultLayout';
+
+const ProfilePage = (): JSX.Element => (
+  <DefaultLayout>
+    <h1>Profile</h1>
+  </DefaultLayout>
+);
+
+export default ProfilePage;
